5c9ad3f6a1a392d717671cd0487b488e724bf703
YDicksonLisa <yanique.dickson@gmail.com>
2020-10-30 16:02:43 -0400

    Added function to calculate years until retired

--- myScript.js
+++ myScript.js
@@ -1,5 +1,6 @@
 var date = 1990;
 var year = 2020;
+var retirementAge = 65;
 var age = function(){
     var myAge = year - date;
     return myAge;
@@ -7,3 +8,12 @@ var age = function(){
 
 currentAge = age()
 console.log('I am ' + currentAge + ' years old.')
+
+var yearsUntilRetire = function() {
+    var years = retirementAge - currentAge;
+    return years
+}
+
+yearsLeft = yearsUntilRetire()
+console.log('I have ' + yearsLeft + ' years until retirement.')
+
